d972672123e2e231fc33f7901e716ac10eb6470a
sadiqonWSL <sadiqonemail@gmail.com>
2023-08-02 14:22:28 +0530

    understanding callbacks

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-console.log("==========================================");
+console.log("======================================");
 
-console.log(aaaa);
-console.log(c());
-console.log(dddd);
-// console.log(bbbb ? bbbb : "where is b?");
-
-var aaaa = 1;
-
-let bbbb = 2;
-
-var dddd = { a: 1, b: 2};
-
-function c() {
-	console.log(z);
-	console.log(this);
-	console.log(this.value);
-	// console.log(x);
-	
-	var z = "z";
-	// let x = 'x';
-	return "hi"
-
+function one() {
+  // cb();
+  console.log("one");
 }
+console.log("flow one")
+function two(cb = one) {
+  cb();
+  console.log("two");
+}
+
+console.log("flow two")
+function three(cb = two) {
+  cb();
+  console.log("three");
+}
+
+console.log("flow three")
+
+three();
+console.log("======================================");
