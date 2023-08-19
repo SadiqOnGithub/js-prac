e72578f58aa58962ef18ef723b50e788e0ba2922
sadiqonWSL <sadiqonemail@gmail.com>
2023-08-19 11:18:46 +0530

    understanding hoisting + script & global scope in callStack

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 console.log("index.js===============================");
 
+// try {
+//   console.dir(aavarX);
+//   console.dir(aaFuncX);
+//   console.dir(letX);
+//   console.dir(constX);
+// } catch (error) {
+//   console.log(error);
+// }
 
+// var aavarX = 10;
+// let letX = 10;
+// const constX = 10;
+// function aaFuncX() {
+//   console.log("inside")
+// }
 
-button.addEventListener("click", () => {
-  Promise.resolve().then(() => console.log('index.js Microtask 1'));
-  console.log('index.js Listner 1');
-})
+window.aaaa = "var-scoped a";
+let aaaa = "lexically-scoped a";
+console.log(aaaa);         // "lexically-scoped a"
+console.log(window.aaaa);  // "var-scoped a"
 
-button.addEventListener("click", () => {
-  Promise.resolve().then(() => console.log('index.js Microtask 2'));
-  console.log('index.js Listner 2');
-})
+let aaab = "lexically-scoped b";
+window.aaab = "var-scoped b";
+console.log(aaab);         // "lexically-scoped b"
+console.log(window.aaab);  // "var-scoped b"
 
-// button.click()
+console.log(aaaa);
+console.log(aaab);
+
+// understand more about script and global scope?. in context of callstack?. of v8?
+// : https://stackoverflow.com/a/70786086/14075519
 
 console.log("index.js===============================");
