7c37feef548f5014cc728d362c3ed9af5fb5bfcd
sadiqonWSL <sadiqonemail@gmail.com>
2023-11-02 08:51:32 +0530

    playing with constructor function

--- index.js
+++ index.js
@@ -1,35 +1,19 @@
 console.log("index.js===============================");
 
-// try {
-//   console.dir(aavarX);
-//   console.dir(aaFuncX);
-//   console.dir(letX);
-//   console.dir(constX);
-// } catch (error) {
-//   console.log(error);
-// }
+function Person(name, age) {
+  this.name = name || 'John';
+  this.age = age || 24;
+  this.displayName = function () {
+    console.log(this.name);
+  };
+}
 
-// var aavarX = 10;
-// let letX = 10;
-// const constX = 10;
-// function aaFuncX() {
-//   console.log("inside")
-// }
+Person.name = 'John';
+Person.displayName = function () {
+  console.log(this.name);
+};
 
-window.aaaa = "var-scoped a";
-let aaaa = "lexically-scoped a";
-console.log(aaaa);         // "lexically-scoped a"
-console.log(window.aaaa);  // "var-scoped a"
-
-let aaab = "lexically-scoped b";
-window.aaab = "var-scoped b";
-console.log(aaab);         // "lexically-scoped b"
-console.log(window.aaab);  // "var-scoped b"
-
-console.log(aaaa);
-console.log(aaab);
-
-// understand more about script and global scope?. in context of callstack?. of v8?
-// : https://stackoverflow.com/a/70786086/14075519
-
+var person = new Person('John');
+person.displayName();
+Person.displayName();
 console.log("index.js===============================");
